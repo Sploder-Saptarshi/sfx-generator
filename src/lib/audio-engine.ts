b710dae357a18b5bd9972dd28fa1af829d4a6236
Geoff Gaudreault <gaudreault@gmail.com>
2026-02-18 16:15:52 +0000

    i wouldn't think we'd event need to "fade out" - it could just stop the

--- src/lib/audio-engine.ts
+++ src/lib/audio-engine.ts
@@ -5,7 +5,7 @@ import { SoundParams, WaveformType, NoiseType, CompositionState, defaultSoundPar
  */
 const NOTE_FREQUENCIES: Record<string, number> = {
   "C3": 130.81, "C#3": 138.59, "D3": 146.83, "D#3": 155.56, "E3": 164.81, "F3": 174.61, "F#3": 185.00, "G3": 196.00, "G#3": 207.65, "A3": 220.00, "A#3": 233.08, "B3": 246.94,
-  "C4": 261.63, "C#4": 277.18, "D4": 293.66, "D#4": 311.13, "E4": 329.63, "F4": 349.23, "F#4": 369.99, "G4": 392.00, "G#4": 415.30, "A4": 440.00, "A#4": 466.16, "B4": 493.88,
+  "C4": 261.63, "C#4": 277.18, "D4": 293.66, "D#4": 311.13, "E4": 329.63, "F4": 349.23, "F#3": 369.99, "G4": 392.00, "G#4": 415.30, "A4": 440.00, "A#4": 466.16, "B4": 493.88,
   "C5": 523.25, "C#5": 554.37, "D5": 587.33, "D#5": 622.25, "E5": 659.25, "F5": 698.46, "F#5": 739.99, "G5": 783.99, "G#5": 830.61, "A5": 880.00, "A#5": 932.33, "B5": 987.77,
 };
 
@@ -404,8 +404,10 @@ class AudioEngine {
       
       const cleanupTime = iterationDuration + params.attack + params.decay + 5;
       setTimeout(() => {
-        activeGains = activeGains.filter(g => g !== masterGain);
-        masterGain.disconnect();
+        if (isPlaying) {
+          activeGains = activeGains.filter(g => g !== masterGain);
+          masterGain.disconnect();
+        }
       }, cleanupTime * 1000);
     };
 
@@ -415,11 +417,11 @@ class AudioEngine {
       stop: () => {
         isPlaying = false;
         activeGains.forEach(g => {
-          if (this.ctx) {
-            g.gain.exponentialRampToValueAtTime(0.0001, this.ctx.currentTime + 0.2);
-            setTimeout(() => g.disconnect(), 300);
-          }
+          try {
+            g.disconnect();
+          } catch (e) {}
         });
+        activeGains = [];
         this.continuousLoops.delete(id);
       }
     });
